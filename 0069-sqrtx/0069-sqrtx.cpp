42446e4acf1d59f2397b67836a01b453a6994cb3
1WHITE-DEVIL <122687762+1WHITE-DEVIL@users.noreply.github.com>
2025-03-04 11:00:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.05%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+long long binarySearch(int n ){
+    int s = 0;
+    int e = n;
+   long long mid = s + (e-s)/2;
+    long long ans = -1;
+    while(s<=e){
+        long long square = mid*mid;
+
+        if (square == n) return mid;
+        if(square < n){
+            ans = mid;
+            s = mid+1;
+        }
+        else {
+            e = mid-1;
+        }
+        mid = s + (e-s)/2;
+
+    }
+    return ans;
+}
+class Solution {
+public:
+    int mySqrt(int x) {
+        return binarySearch(x);
+    }
+};
