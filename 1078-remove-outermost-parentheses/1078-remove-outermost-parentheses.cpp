dc9fc3bc7a7f22be6de102c993565b3f1c87b08c
1WHITE-DEVIL <122687762+1WHITE-DEVIL@users.noreply.github.com>
2025-04-18 15:57:24 +0530

    Time: 0 ms (100%), Space: 9.1 MB (26.12%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int balance  = 0;
+        string result;
+        for (auto ch : s){
+            if(ch == '(') {
+                if(balance > 0 )  result += ch;
+                balance++;
+            }
+            else{ 
+                balance--; 
+                if(balance>0) result += ch;
+               
+            }
+        }
+    return result;
+    }
+};
