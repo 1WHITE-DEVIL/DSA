5c3f2a54b2aca24acf319364fca5efe4c0558676
1WHITE-DEVIL <122687762+1WHITE-DEVIL@users.noreply.github.com>
2025-12-19 11:53:12 +0530

    Time: 51 ms (56.43%), Space: 90.8 MB (44.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,7 +2,14 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
-        return unordered_set<int>(nums.begin(), nums.end()).size() < nums.size();
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+            seen.insert(num);
+        }
+        return false;
 
     }
 };
