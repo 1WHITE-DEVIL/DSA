ad28a350b9801b76cedb2d510a85d5c91b5a93d7
1WHITE-DEVIL <122687762+1WHITE-DEVIL@users.noreply.github.com>
2026-01-04 14:23:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.41%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,44 +10,27 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* list1 , ListNode* list2 ){
-        if(list1 == nullptr) return list2;
-        if(list2 == nullptr) return list1;
-        if(list1-> next == nullptr){
-            list1 -> next = list2;
-            return list1;
-        }
-        ListNode* curr1 = list1;
-        ListNode* next1 = curr1 -> next;
-        ListNode* curr2 = list2;
-        ListNode* next2 = curr2 -> next;
-
-        while(next1 != nullptr && curr2 != nullptr){
-            if(curr2 -> val >= curr1 -> val && curr2 -> val <= next1 -> val){
-                curr1 -> next = curr2 ;
-                next2 = curr2 -> next;
-                curr2 -> next = next1;
-                curr1 = curr2;
-                curr2 = next2;
-            }
-        else{
-            curr1 = next1;
-            next1 = next1-> next;
-            if(next1 == nullptr){
-                curr1-> next = curr2;
-                return list1;
-            }
-        }
-        }
-    return list1;
-    }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == nullptr) return list2;
-        if(list2 == nullptr) return list1;
+        ListNode dummy(0);
+        ListNode* node = &dummy;
 
-        if(list1 ->val <= list2 -> val){
-            return solve(list1 , list2);
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                node->next = list1;
+                list1 = list1->next;
+            } else {
+                node->next = list2;
+                list2 = list2->next;
+            }
+            node = node->next;
         }
-        else return solve(list2 , list1);
+
+        if (list1) {
+            node->next = list1;
+        } else {
+            node->next = list2;
+        }
+
+        return dummy.next;
     }
 };
