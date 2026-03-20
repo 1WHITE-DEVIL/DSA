25e43bad7ed463ef7058419ed68d6e084046629a
Aditya Gupta <122687762+1WHITE-DEVIL@users.noreply.github.com>
2026-03-20 22:58:32 +0530

    Time: 62 ms (48.82%), Space: 130.3 MB (49.61%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    int Sum[2][1001]; int seenX[2][1001];
     int numberOfSubmatrices(vector<vector<char>>& grid) {
-        const int r=grid.size(), c=grid[0].size();
-        memset(Sum[0], 0, (c+1)*sizeof(int));
-        memset(Sum[1], 0, (c+1)*sizeof(int));
-        memset(seenX[0], 0, (c+1)*sizeof(int));
-        memset(seenX[1], 0, (c+1)*sizeof(int));
-        int cnt=0;
-        for(int i=0; i<r; i++){
-            bool iEven=(i&1)==0, prv=!iEven;
-            for(int j=0; j<c; j++){
-                const char c=grid[i][j];
-                bool isX=c=='X', isY=c=='Y';
-                Sum[iEven][j+1]=isX-isY+Sum[iEven][j]+Sum[prv][j+1]-Sum[prv][j];
-                seenX[iEven][j+1]=isX+seenX[iEven][j]+seenX[prv][j+1]-seenX[prv][j];
-                cnt+=((Sum[iEven][j+1]==0) & (seenX[iEven][j+1]>0));
+         int n = grid.size(), m = grid[0].size();
+    
+    vector<vector<int>> sum(n, vector<int>(m, 0));
+    vector<vector<int>> countX(n, vector<int>(m, 0));
+    
+    int ans = 0;
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            
+            int val = 0;
+            if(grid[i][j] == 'X') val = 1;
+            else if(grid[i][j] == 'Y') val = -1;
+            
+            sum[i][j] = val;
+            countX[i][j] = (grid[i][j] == 'X');
+            
+            if(i > 0) {
+                sum[i][j] += sum[i-1][j];
+                countX[i][j] += countX[i-1][j];
+            }
+            if(j > 0) {
+                sum[i][j] += sum[i][j-1];
+                countX[i][j] += countX[i][j-1];
+            }
+            if(i > 0 && j > 0) {
+                sum[i][j] -= sum[i-1][j-1];
+                countX[i][j] -= countX[i-1][j-1];
+            }
+            
+            // check valid submatrix from (0,0) to (i,j)
+            if(sum[i][j] == 0 && countX[i][j] > 0) {
+                ans++;
             }
         }
-        return cnt;
+    }
+    
+    return ans;
     }
 };
