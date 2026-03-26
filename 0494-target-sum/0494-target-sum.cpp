6f5bc4fd2d081aee24d15ed932657561d2827637
Aditya Gupta <122687762+1WHITE-DEVIL@users.noreply.github.com>
2026-03-26 22:53:44 +0530

    Time: 0 ms (100%), Space: 12.3 MB (71.05%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+    int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+    if ((target + totalSum) % 2 != 0 || abs(target) > totalSum)
+        return 0;
+
+    int P = (target + totalSum) / 2;
+
+    vector<int> dp(P + 1, 0);
+    dp[0] = 1;
+
+    for (int num : nums) {
+        for (int j = P; j >= num; j--) {
+            dp[j] += dp[j - num];
+        }
+    }
+
+    return dp[P];  
+    }
+};
