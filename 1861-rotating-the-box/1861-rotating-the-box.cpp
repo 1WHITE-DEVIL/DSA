10b32492d373e9ee3c7cd44478c270bc78f1b36e
Aditya Gupta <122687762+1WHITE-DEVIL@users.noreply.github.com>
2026-05-06 22:21:09 +0530

    Time: 16 ms (33.91%), Space: 56.7 MB (90.17%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
-        const int r=boxGrid.size(), c=boxGrid[0].size();
-        vector<vector<char>> rotate(c, vector<char>(r, '.'));
-        for(int i=0; i<r; i++){
-            int bottom=c-1;
-            for(int j=c-1; j>=0; j--){
-                switch (boxGrid[i][j])
-                {
-                case '#':
-                    rotate[bottom][r-1-i]='#';
-                    bottom--;
-                    break;
-                case '*':
-                    rotate[j][r-1-i]='*';
-                    bottom=j-1;
-                    break;
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+        // Step 1: simulate gravity (right side)
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                } 
+                else if (boxGrid[i][j] == '#') {
+                    swap(boxGrid[i][j], boxGrid[i][empty]);
+                    empty--;
                 }
             }
         }
-        return rotate; 
+        vector<vector<char>> res(n, vector<char>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+
+        return res;
     }
 };
