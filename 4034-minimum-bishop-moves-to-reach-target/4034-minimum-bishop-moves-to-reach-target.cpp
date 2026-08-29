bae9842739cad4bfe7b80511d09144aa808330cb
Aditya Gupta <122687762+1WHITE-DEVIL@users.noreply.github.com>
2026-08-30 01:19:58 +0530

    Time: 0 ms (100%), Space: 50.6 MB (0%) - LeetHub

---
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minBishopMoves(vector<int>& source, vector<int>& target) {
+        int x = accumulate(source.begin(), source.end(),0);
+        int y = accumulate(target.begin(),target.end(),0);
+        if(abs(source[0]-target[0]) == abs(source[1] - target[1]) ) return 1;
+        if(x%2==y%2) return 2;
+        return -1;
+    }
+};
